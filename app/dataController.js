11c2a0d3e143103318cbde73ff7f4bc5df6365fb
Daniel Falabella <dnlfala@gmail.com>
2015-12-04 10:42:53 -0700

    Refactored check for unvalid user code.

--- app/dataController.js
+++ app/dataController.js
@@ -55,7 +55,7 @@ angular.module('trello-person')
       // GET ID OF ALL USERS IN ARRAY
       board.memberships.forEach(function(member) {
         // Don't account for managers
-        if (member.idMember == '54934bdddfd54cb2c7f37010' || member.idMember == '52d84fcd9595f66b5b68801d' || member.idMember == '4f552ff2b0ca8f2217fc7d38' || member.idMember == '53c3772a84b64a8c50441911' || member.idMember == '5474c41bf9c8688c5ec07390' || member.idMember == '564a076e6c7e55890c4af0f1' || member.idMember == '557976153d63ef846e16a992' || member.idMember == '4e6a7fad05d98b02ba00845c') return;
+         if (checkInvalidMember(member.idMember)) return;
 
         if (users.indexOf(member.idMember) < 0) {
           users.push(member.idMember);
@@ -79,6 +79,17 @@ angular.module('trello-person')
     userIds = users;
     return users;
   };
+
+  function checkInvalidMember (memberId) {
+    var nonAllowedMembers = [
+      '54934bdddfd54cb2c7f37010', '52d84fcd9595f66b5b68801d', '4f552ff2b0ca8f2217fc7d38', '53c3772a84b64a8c50441911',
+      '5474c41bf9c8688c5ec07390', '564a076e6c7e55890c4af0f1', '557976153d63ef846e16a992', '4e6a7fad05d98b02ba00845c',
+      '560177cc7f811c3a8f2ac82b', '544727553007a090c3d02f7c', '560177385015d0908f5964ea', '56017726a92657be642536c3',
+      '55fb8cd18615dde67bdd09b6', '55f85767d8ddcd153ed0baf1', '55f1f8466c4096eee9d49dab'
+    ];
+    if (nonAllowedMembers.indexOf(memberId) > -1) return true;
+    return false;
+  }
 
   /////////////////////////////////////////////////////////
 
